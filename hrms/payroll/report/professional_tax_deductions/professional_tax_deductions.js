83183d87d5474e5829dc12a724637797988c1b1e
samar-elgmal <samar.elgmal@gmail.com>
2023-10-09 23:25:41 +0300

    first commit

--- hrms/payroll/report/professional_tax_deductions/professional_tax_deductions.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.require("assets/hrms/js/salary_slip_deductions_report_filters.js", function() {
-	frappe.query_reports["Professional Tax Deductions"] = hrms.salary_slip_deductions_report_filters;
-});
--- hrms/public/js/hrms.bundle.js
+++
@@ -1,2 +0,0 @@
-import "./templates/employees_with_unmarked_attendance.html";
-import "./utils";
